1de770f3a900c0d515a7a05e8a931cf3fdd0997d
Yello <imnotluis38@gmail.com>
2022-07-06 12:24:24 -0500

    add warning

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,11 +24,11 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"Hey there!\n
+			Please don't spam my comments about mobile controls.!\n
+			Instead, load up a song. Exit out of that song and.\n
+			change your mobile controls. It's weird, I know.\n
+			Thanks for playing!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
